190fa2e3837eb8ed2c2f9607caeebc84e5d35fe3
Behzad Koohi <behzadk@ca.ibm.com>
2022-06-02 14:59:38 -0400

    Update main.tf

--- IBM_Cloud/singleVM-VPC/main.tf
+++ IBM_Cloud/singleVM-VPC/main.tf
@@ -73,7 +73,7 @@ resource "ibm_is_instance" "cam-server" {
 
   vpc  = ibm_is_vpc.cam_vpc.id
   zone = var.zone
-  keys = [ibm_is_ssh_key.cam_sshkey.id]
+  keys = [data.ibm_is_ssh_key.cam_sshkey.id]
   tags = module.camtags.tagslist
 }
 
